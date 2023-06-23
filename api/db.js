429453a2faba02858a697ad41af6fce5216872cc
AnirbanOne <anirban.chk1@gmail.com>
2023-06-23 13:44:00 +0530

    gitignore update

--- api/db.js
+++ api/db.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import mysql from "mysql2";
 
 export const db = mysql.createConnection({
-    host: 'database-1.csclha9qbzz8.ap-south-1.rds.amazonaws.com', // Replace with your RDS host
+    host: "database-1.csclha9qbzz8.ap-south-1.rds.amazonaws.com", // Replace with your RDS host
     user: process.env.DB_USER, // Assuming you have set the DB_USER value in your .env file
     password: process.env.DB_PASSWORD, // Assuming you have set the DB_PASSWORD value in your .env file
     database: process.env.DB_NAME, // Assuming you have set the DB_NAME value in your .env file
